924b8072d1afb1460d2502279c61fa363103a353
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2022-10-25 10:53:29 -0700

    hook: removing CLI hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -22,10 +22,6 @@ def add_symlink(path: Path, target: Union[str, Path], target_is_directory: bool 
 
 if __name__ == "__main__":
 
-    if "No command-line interface" in "{{ cookiecutter.command_line_interface }}":
-        remove_file(PROJECT_TESTS / "test_cli.py")
-        remove_file(PROJECT_SRC / "cli.py")
-
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE.rst")
     else:
